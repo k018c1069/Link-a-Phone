85dc58f0711c353a2db6b1b3d69c53f07f1f699c
hingashi-coder <yukito@toukoutounoMacBook-Air.local>
2020-11-12 14:23:57 +0900

    実装

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ if (!process.env.PORT) {
 admin.initializeApp(settings)
 const fireStore = admin.firestore()
 const LineAuthWaiting = fireStore.collection('LineAuthWaiting')
+const UserDataRef = fireStore.collection('UserData')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
@@ -26,32 +27,60 @@ app.use(function(req, res, next) {
   next()
 })
 
-app.post('/', (req, res) =>{
-  const token = req.body.token
-  try {
-    admin.auth().verifyIdToken(token,true).then(auth => {
-      res.json({
-        auth: 'OK'
-      })
-    }).catch(err => {
-      res.json({
-        auth: 'NG'
-      })
-    })
-  } catch(e) {
-    res.json({
-      error: e
-    })
-  }
-  
-})
 
-app.get('/line', () => {
-  const LineData = LineAuthWaiting.where(
-    'authCode', '==', '1604335208545'
-  ).get()
-  LineData.then(e => {
-    console.log(e.docs[0]);
+
+const verifyUser = (token) => {
+  return new Promise((resolve, reject) => {
+    admin.auth().verifyIdToken(token,true).then(auth => {
+      resolve(auth.user_id)
+    }).catch(() => {
+      reject()
+    })
+  })
+}
+
+app.post('/line', (req, res) => {
+  verifyUser(req.body.token).then(userID => {
+    const LineData = LineAuthWaiting.where(
+      'authCode', '==', req.body.authCode
+    ).get()
+    LineData.then(e => {
+      if (e.size > 0) {
+        const data = e.docs[0].data()
+        res.json({
+          status: 'OK'
+        })
+        UserDataRef.doc(userID).set(
+          {
+            lineUUID: data.lineUUID
+          }
+        ,{merge: true})
+        LineAuthWaiting.doc(e.docs[0].id).delete() 
+      } else {
+        res.json({
+          status: 'error',
+          msg: 'コードが見つかりません。'
+        })
+      }
+    })
+  }).catch(() => {
+    res.send(403)
+  })
+})
+app.post('/device', (req, res) => {
+  verifyUser(req.body.token).then(userID => {
+    UserDataRef.doc(userID).set({
+      DeviceID: req.body.DeviceID,
+      postCustomName: req.body.postCustomName
+    },{merge: true})
+    res.json({
+      status: 'OK'
+    })
+  }).catch(() => {
+    res.json({
+      status: 'error',
+      msg: 'デバイスが見つかりません。'
+    })
   })
 })
 
